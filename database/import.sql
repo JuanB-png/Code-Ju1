6b63413498ebe57e0209fbecdeb64be20d0bf817
JuanB-png <114988701+JuanB-png@users.noreply.github.com>
2023-06-22 09:55:54 +0200

    Get out of database in index

---
+++ database/import.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE code_ju1;
+
+USE code_ju1;
+
+CREATE TABLE code (
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	title varchar(255) NOT NULL,
+    code TEXT NOT NULL,
+    uploaddate DATE
+);

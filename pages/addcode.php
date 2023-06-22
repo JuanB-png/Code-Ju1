e63f13bf2a8c1ac5e38ace5f04c0144883604381
Emiel Liefhebber <171208@student.horizoncollege.nl>
2023-06-22 10:38:07 +0200

    connections to functions in add

---
+++ pages/addcode.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("database/dbconnect.php");
+require_once("components/functions.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <form method="post">
+
+
+    
+    </form>
+</body>
+</html>

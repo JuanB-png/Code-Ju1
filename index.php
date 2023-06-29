f8e7751a27042e78fb037251ac08bc9553610e83
gebakkenbraatboter <171262@student.horizoncollege.nl>
2023-06-29 11:13:08 +0200

    nav bar update

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $true_or_flase = true;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link id="lighttheme" rel="stylesheet" href="style/stylelight.css">
     <title>Home</title>
     <script src="script/script.js" defer></script>
 </head>
